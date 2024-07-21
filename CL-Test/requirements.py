552a015f97ab1f7ab2913e6ba1b16e9c5e324e81
rakechen-0307 <rakechen168@gmail.com>
2024-07-21 18:49:10 +0800

    update requirements

--- CL-Test/requirements.py
+++ CL-Test/requirements.py
@@ -1,9 +1,7 @@
 import os
 
-os.system("git clone https://github.com/vaibhavs10/CLAP")
-os.system("cd CLAP && pip install .")
+os.system("git clone https://github.com/facebookresearch/ImageBind.git")
+os.system("cd ImageBind && pip install .")
 os.system("pip install pydub ffmpeg torchaudio_augmentations soundfile")
 os.system("conda install -c conda-forge sox")
-os.system("pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu121")
-os.system("pip install --upgrade transformers==4.30.0")
-os.system("wget https://huggingface.co/lukewys/laion_clap/resolve/main/music_audioset_epoch_15_esc_90.14.pt")
+os.system("pip install tqdm")
