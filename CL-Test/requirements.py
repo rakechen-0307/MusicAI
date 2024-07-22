dbfe5d66bb3872044c7025b0d668afad0fa1e74e
rakechen-0307 <rakechen168@gmail.com>
2024-07-22 08:59:38 +0800

    update requirements

--- CL-Test/requirements.py
+++ CL-Test/requirements.py
@@ -1,7 +1,9 @@
 import os
 
+os.system("conda install numpy ffmpeg")
 os.system("git clone https://github.com/facebookresearch/ImageBind.git")
 os.system("cd ImageBind && pip install .")
 os.system("pip install pydub ffmpeg torchaudio_augmentations soundfile")
 os.system("conda install -c conda-forge sox")
 os.system("pip install tqdm")
+os.system("pip install filelock")
